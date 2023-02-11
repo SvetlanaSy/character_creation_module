2af77486d4c89c3493f5977d278e37177b77acb2
Svetlana Sycheva <svetlanasych@gmail.com>
2023-02-11 14:32:32 +0200

    Add docstring

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 from random import randint
 
 
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
+    """Damage to oponent."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 '{5 + randint(3, 5)}')
@@ -16,6 +20,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Defence against damage."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -26,6 +31,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Apply special skills."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 '{80 + 25}»')
@@ -37,6 +43,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Training special skills."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -61,9 +68,10 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Choose character."""
     approve_choice: str = ''
     char_class: str = ''
-    while approve_choice != 'Y':
+    while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого хочешь '
                            'играть: Воитель — warrior, '
                            'Маг — mage, Лекарь — healer: ')
@@ -82,7 +90,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__== '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -94,4 +103,4 @@ def main():
     print(start_training(char_name, char_class))
 
 
-main()
+
